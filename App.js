aa7c73f343f4284fcc1a217e5c69f9ce0045eef2
Raj Verma <info.mrverma@gmail.com>
2020-06-21 10:50:42 +0530

    01 class

--- App.js
+++ App.js
@@ -1,19 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import {View, Text, StyleSheet,Button,Alert,TouchableOpacity } from "react-native"
+
+export default class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            randomColor:null
+        }
+    }
+
+    getRandomColor = () =>{
+        return(
+            "rgb("+
+            Math.floor(Math.random() * 256) + "," +
+            Math.floor(Math.random() * 256) + "," +
+            Math.floor(Math.random() * 256) + ")"
+        )
+    }
+
+    myButtonPress = () => {
+        this.setState({randomColor : this.getRandomColor() });
+    }
+
+    render() {
+        return (
+            <View style={[styles.container,{backgroundColor : this.state.randomColor}]}>
+                <TouchableOpacity
+                onPress={this.myButtonPress}
+                >
+                    <Text style={styles.text}>Login</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#c1c1c1",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    text:{
+        fontSize:30,
+        backgroundColor: "#25CCF7",
+        paddingVertical:12,
+        paddingHorizontal:40,
+        color:"#ffffff",
+        borderRadius:10,
+        borderWidth:3,
+        borderColor:"#fff"
+    }
 });
